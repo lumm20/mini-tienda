7ff9be33f79987234d0650815320611dfb3ce828
luisa <moraLF20@outlook.com>
2025-12-05 14:59:49 -0700

    agregando validador para actualizar productos

--- backend/src/middlewares/validators/productValidator.js
+++ backend/src/middlewares/validators/productValidator.js
@@ -1,5 +1,38 @@
 import { body, param } from "express-validator";
 
+const validateUpdateProduct = () => {
+    return [
+        body('id')
+            .not().exists().withMessage('No puedes modificar el ID del producto'),
+        body('name')
+            .optional({ values: 'falsy' })
+            .trim()
+            .notEmpty().withMessage('El nombre no puede estar vacío')
+            .bail()
+            .isLength({ max: 50 }).withMessage('El nombre no debe exceder los 50 caracteres')
+            .escape(),
+        body('description')
+            .optional({ values: 'falsy' })
+            .trim()
+            .notEmpty().withMessage('La descripción no puede estar vacía')
+            .bail()
+            .isLength({ max: 200 }).withMessage('La descripción no debe exceder los 200 caracteres')
+            .escape(),
+        body('price')
+            .optional({ values: 'falsy' })
+            .trim()
+            .notEmpty().withMessage('El precio no puede venir vacío')
+            .bail()
+            .isDecimal({ decimal_digits: '0,2' }).withMessage('El precio debe ser un número decimal válido con hasta 2 decimales'),
+        body('stock')
+            .optional({ values: 'falsy' })
+            .trim()
+            .notEmpty().withMessage('El stock no puede venir vacío')
+            .bail()
+            .isInt({ min: 0 }).withMessage('El stock debe ser un número entero mayor o igual a 0')
+    ];
+};
+
 const validateProduct = () => {
     return [
         body('name')
@@ -29,4 +62,4 @@ const validateProduct = () => {
 
 const validateProductId = ()=>param('id').isInt({min:1}).withMessage('El ID es inválido');
 
-export { validateProduct, validateProductId };
+export { validateProduct, validateProductId, validateUpdateProduct };
